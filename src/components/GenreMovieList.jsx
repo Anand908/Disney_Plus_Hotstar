0258aa8192e26431a58e2034ce8b9e8d8a84a23e
Anand908 <anandk212307@gmail.com>
2023-09-09 13:51:19 +0530

    second commit

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -5,7 +5,7 @@ import MovieList from './MovieList';
 function GenreMovieList() {
   return (
     <div>
-        {GenresList.genere.map((item, index) => (
+        {GenresList.genere.map((item, index) => index!=5 && (
             <div key={index} className='md:px-16'>
                 <h2 className='text-[20px] text-white font-bold px-8'>{item.name}</h2>
 
